e84de8395e8c32402930fa142e71ed1d61301fc7
zingxy <zingxy@users.noreply.github.com>
2022-07-31 14:26:59 +0000

    deploy: a7546d046734e8ab23993172e58e5b0ec7e07c22

---
+++ assets/index.md.7eabd372.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a,a as n}from"./app.7fdab5a7.js";const u=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"index.md"}'),o={name:"index.md"},s=n("p",null,"123456",-1),r=[s];function c(d,_,i,p,l,m){return a(),t("div",null,r)}var x=e(o,[["render",c]]);export{u as __pageData,x as default};
